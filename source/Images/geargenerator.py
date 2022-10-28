ed8453a1cb91e521522b7f16216935196e7f3db2
IceCube Solutions <martino@uia.no>
2022-10-28 15:15:08 +0200

    Create geargenerator.py

---
+++ source/Images/geargenerator.py
@@ -0,0 +1,32 @@
+'''
+Gear Generator MAS417 Project
+Flexible Production
+Main class
+Martin Økter, Per Henrik, Torbjørn
+28.10.2022
+'''
+
+class GearGenerator:
+    def __init__(self):
+        self.next = 0
+
+    def inputFase(self):
+        return 0
+
+    def meshFase(self):
+        return 0
+
+    def outputFase(self):
+        return 0
+
+    def pointFase(self):
+        return 0
+
+    def returnNext(self):
+        return 0
+
+    def setNext(self):
+        return 0
+
+    def stlFase(self):
+        return 0
