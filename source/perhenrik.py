407207b537ddac4e6a034bfd3d86716a1722e511
Per Henrik Hardeberg <perhenrikh@hotmail.com>
2022-11-03 16:56:44 +0100

    g

--- source/perhenrik.py
+++ source/perhenrik.py
@@ -5,30 +5,9 @@ Per Henrik Hardeberg
 26.10.2022
 '''
 
-import pointgenerator as pg
-import matplotlib.pyplot as plt
-
-specificationList = [5, 20, 3,  3,20, 200,8.3540, 58.2250]
-        # [innerDiameter, outerDiameter, teethheight,gearheight, nuberOfTeeth, printTemperature [C],  locationLat, locationLong]
-ring = pg.PointGenerator(specificationList)
-
-
-ax = plt.axes(projection='3d')
-plt.plot(ring.pointsInner[0],ring.pointsInner[1],ring.pointsInner[2], '*')
-plt.plot(ring.xMain, ring.yMain, ring.zMain, '*')
-plt.plot(ring.xOuter, ring.yOuter, ring.zOuter, '*')
-
-plt.plot(ring.xInnerOffset, ring.yInnerOffset, ring.zInnerOffset, '*')
-plt.plot(ring.xMainOffset, ring.yMainOffset, ring.zMainOffset, '*')
-plt.plot(ring.xOuterOffset, ring.yOuterOffset, ring.zOuterOffset, '*')
-plt.show()
-
-print(ring.meanTemp)
-
-
 # TO DO Per Henrik:
 # # Correct API to find mean. Not sure about the dates and the mean value
-# # Calculate and correct the size of the gear!
+# # Calculate and correct the size of the gear! Think this is OK
 # # Help with the mesh?
 # # Update class diagram because API is a function inside PointGeneratir
 
