cfd048d52b464c18388eff07ae613acb481372b5
IceCube Solutions <martino@uia.no>
2022-11-10 12:03:30 +0100

    Correction of fase loop

--- source/geargenerator.py
+++ source/geargenerator.py
@@ -16,11 +16,14 @@ uApi = userapi.UserApi()
 class GearGenerator:
     def __init__(self):
         self.next = 0
-        self.spesificationList = []
+        self.spesificationList = [10, 40, 2,  2, 20, 200,8.3540, 58.2250]
         self.temp = 0
         self.pointsInner = []
 
     def inputFase(self):
+        '''
+        Fase for reciving user input for wanted size of the gear
+        '''
         action = uApi.userInput()
         if action:
             self.spesificationList = uApi.getList()
@@ -28,6 +31,9 @@ class GearGenerator:
         return 0
 
     def meshFase(self):
+        '''
+        Fase for creating a mesh from the reciving point cloud and save as .stl file
+        '''
         action = uApi.generatingPMS(self.temp)
         action = True
 
@@ -48,17 +54,26 @@ class GearGenerator:
         return 0
 
     def outputFase(self):
+        '''
+        Feedback for succsesfull gear generating and usefull information
+        '''
         action = uApi.outputPage()
         if action:
             self.next = 0
         return 0
 
     def pointFase(self):
-        uApi.fetchingTemperature()
+        '''
+        Fase for genrerating th epointcloud from the user data and preset parameters
+        '''
+        # Keyword if loop shal be used
         action = True
-        dummyList = [10, 40, 2,  2, 20, 200,8.3540, 58.2250]
+
+        #Rertriving location mean temperature
+        uApi.fetchingTemperature()
+
         # [innerDiameter, outerDiameter, teethheight,gearheight, nuberOfTeeth, printTemperature [C],  locationLat, locationLong]
-        self.pGen = pg.PointGenerator(dummyList) #Her oprettes objektet, bytt til spesification list når den skal testes skikkelig.
+        self.pGen = pg.PointGenerator(self.spesificationList) #Her oprettes objektet, bytt til spesification list når den skal testes skikkelig.
         self.temp = self.pGen.returnTemp()
         # Per
         if action:
@@ -66,6 +81,9 @@ class GearGenerator:
         return 0
 
     def mainLoop(self):
+        '''
+        Main loop for selection of fase and corensponding fase
+        '''
         if self.next == 0:
             self.inputFase()
         elif self.next == 1:
